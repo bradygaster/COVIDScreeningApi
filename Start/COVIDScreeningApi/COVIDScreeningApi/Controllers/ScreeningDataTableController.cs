f26b293e6129a2a142925d191299ca501dda75cc
Brady Gaster <bradyg@microsoft.com>
2020-04-17 01:05:28 -0700

    fixed api paths by removing "api" from them

--- Start/COVIDScreeningApi/COVIDScreeningApi/Controllers/ScreeningDataTableController.cs
+++ Start/COVIDScreeningApi/COVIDScreeningApi/Controllers/ScreeningDataTableController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace COVIDScreeningApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ScreeningDataTableController : ControllerBase
     {
