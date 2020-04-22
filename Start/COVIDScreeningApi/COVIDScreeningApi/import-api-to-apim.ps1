f505763d2715511d199445f7dde6d2f63b3603c5
Brady Gaster <bradyg@microsoft.com>
2020-04-22 02:15:10 -0700

    changed things back to on build. changed log so that it goes to stdout rather than to a text file we delete, so we can see what's happening for this demo.

---
+++ Start/COVIDScreeningApi/COVIDScreeningApi/import-api-to-apim.ps1
@@ -0,0 +1,33 @@
+﻿cls
+
+# Variables
+Set-Variable -Name ResourceGroupName "COVIDScreeningAPI"
+Set-Variable -Name ApimInstance "screening-app-apis"
+Set-Variable -Name SwaggerFilePath "C:\Users\brady\source\repos\bradygaster\COVIDScreeningApi\Start\COVIDScreeningApi\COVIDScreeningApi\bin\Debug\netcoreapp3.1\swagger.json"
+Set-Variable -Name ServiceUrl "https://covid-screening-api.azurewebsites.net"
+Set-Variable -Name ApiId "COVIDScreeningApi"
+Set-Variable -Name ApiVersion "v1"
+Set-Variable -Name AzureSubscriptionId "52462f3d-9226-4c1a-ae95-9a1f2b2c2e0f"
+
+# select the subscription
+Set-AzContext -SubscriptionId $AzureSubscriptionId
+
+# import the api
+Write-Output "Setting API Management Context"
+$ApiMgmtContext = New-AzApiManagementContext -ResourceGroupName $ResourceGroupName -ServiceName $ApimInstance
+
+# remove the old api
+Write-Output "Removing the old API"
+Remove-AzApiManagementApi -Context $ApiMgmtContext -ApiId $ApiId
+
+# import the new api
+Write-Output "Importing Swagger into a new API"
+Import-AzApiManagementApi -Context $ApiMgmtContext -ApiId $ApiId -ServiceUrl $ServiceUrl -SpecificationFormat "Swagger" -SpecificationPath $SwaggerFilePath -Path $ApiVersion
+
+# get the api
+$api = Get-AzApiManagementApi -Context $ApiMgmtContext -ApiId $ApiId
+
+# disable the subscription requirement
+Write-Output "Disabling the subscription header requirement" 
+$api.SubscriptionRequired = $false
+Set-AzApiManagementApi -InputObject $api
